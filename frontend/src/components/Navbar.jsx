7c1905dabc729260ae11ddd92fd9d9a4d2f541fc
Raimo Kivi <113283931+F1Zuwu@users.noreply.github.com>
2026-02-21 14:39:16 +0200

    little bit of homepage

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,57 @@
+import langIco from "../assets/icons/lang.png"
+
+const Navbar = () => {
+    return (
+        <div class="bg-black text-white mt-12">
+            <div class="h-24 flex items-center px-10">
+                <h1 class="text-3xl">Ettevõtlikkuse Pass</h1>
+                <div class="absolute right-10 flex items-center  ">
+                    <img src={langIco} class="w-5 h-5 mr-2" alt="Language Icon"></img>
+                    <button>Eesti keel</button>
+
+                    <svg class="ml-3" xmlns="http://www.w3.org/2000/svg" width="10" height="9" viewBox="0 0 10 9" fill="none">
+                        <path d="M4.82053 8.34939L9.64106 -2.86102e-06H1.00136e-05L4.82053 8.34939Z" fill="white" />
+                    </svg>
+                </div>
+            </div>
+            <div class="px-10 py-4 flex gap-20 relative">
+                <div class="flex items-center">
+                    <a>Avaleht</a>
+                    <svg class="ml-3" xmlns="http://www.w3.org/2000/svg" width="10" height="9" viewBox="0 0 10 9" fill="none">
+                        <path d="M4.82053 8.34939L9.64106 -2.86102e-06H1.00136e-05L4.82053 8.34939Z" fill="white" />
+                    </svg>
+                </div>
+                <div class="flex items-center">
+                    <a>Ettevõtlikkuse Pass</a>
+                    <svg class="ml-3" xmlns="http://www.w3.org/2000/svg" width="10" height="9" viewBox="0 0 10 9" fill="none">
+                        <path d="M4.82053 8.34939L9.64106 -2.86102e-06H1.00136e-05L4.82053 8.34939Z" fill="white" />
+                    </svg>
+                </div>
+                <div class="flex items-center">
+                    <a>Funktsioonid</a>
+                    <svg class="ml-3" xmlns="http://www.w3.org/2000/svg" width="10" height="9" viewBox="0 0 10 9" fill="none">
+                        <path d="M4.82053 8.34939L9.64106 -2.86102e-06H1.00136e-05L4.82053 8.34939Z" fill="white" />
+                    </svg>
+                </div>
+                <div class="flex items-center">
+                    <a>Abi ja Kontakt</a>
+                    <svg class="ml-3" xmlns="http://www.w3.org/2000/svg" width="10" height="9" viewBox="0 0 10 9" fill="none">
+                        <path d="M4.82053 8.34939L9.64106 -2.86102e-06H1.00136e-05L4.82053 8.34939Z" fill="white" />
+                    </svg>
+                </div>
+
+                <div class="absolute right-10 flex top-2">
+                    <button class="px-4 py-2 bg-white text-black flex items-center gap-2 rounded-full">
+                        <h1 class="mr-6">Logi sisse</h1>
+                        <svg class="absolute right-4 top-3" xmlns="http://www.w3.org/2000/svg" width="17" height="25" viewBox="0 0 17 30" fill="none">
+                            <ellipse cx="8.6857" cy="5.49032" rx="5.21146" ry="5.49032" fill="black" />
+                            <path d="M16.9372 19.2161C16.9372 17.8573 16.7182 16.5118 16.2926 15.2564C15.867 14.0011 15.2432 12.8604 14.4568 11.8996C13.6705 10.9388 12.7369 10.1766 11.7094 9.6566C10.682 9.13661 9.58074 8.86897 8.46862 8.86897C7.35651 8.86897 6.25528 9.13661 5.22782 9.6566C4.20036 10.1766 3.26679 10.9388 2.4804 11.8996C1.69402 12.8604 1.07022 14.0011 0.644635 15.2564C0.219047 16.5118 -9.72242e-08 17.8573 0 19.2161L8.46862 19.2161H16.9372Z" fill="black" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar; 
--- frontend/src/container/Frontpage/Home.jsx
+++ frontend/src/container/Frontpage/Home.jsx
@@ -1,6 +1,30 @@
+import Navbar from "../../components/Navbar";
+import homeImg from "../../assets/images/banner.png"
 const Home = () => {
-    return(
-        <div></div>
+    return (
+        <div>
+            <Navbar></Navbar>
+
+            <div class="p-9 flex">
+                <div class="bg-black px-8 py-9 w-1/2">
+                    <h1 class="text-white text-6xl">Ettevõtlikkuse Pass</h1>
+                    <h2 class="text-white text-4xl">Sinu idee, meie tugi!</h2>
+                    <p class="text-white text-base mt-5">Sinu personaalne tööriist enesearengu
+                        jälgimiseks ja tööturule kandideerimiseks</p>
+                    <button>
+                        Loo Ettevõtlikkuse passi kasutaja
+                    </button>
+                    <button>
+                        Ettevõtlikkuse passi tööriistad
+                    </button>
+                    <a>Halda oma passi {">"} </a>
+                    <a>Halda oma tegevusi {">"} </a>
+                </div>
+                <div class="w-1/2">
+                    <img class="w-full" src={homeImg}></img>
+                </div>
+            </div>
+        </div>
     )
 }
 
